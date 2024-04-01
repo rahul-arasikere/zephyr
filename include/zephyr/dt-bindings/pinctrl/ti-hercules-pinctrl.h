7bc263b5a873100a68533870de6d980ae860e635
Rahul Arasikere <arasikere.rahul@gmail.com>
2024-04-01 19:56:21 -0400

    added a basic board definition for testing

---
+++ include/zephyr/dt-bindings/pinctrl/ti-hercules-pinctrl.h
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2024 Rahul Arasikere <arasikere.rahul@gmail.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_TI_HERCULES_PINCTRL_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_TI_HERCULES_PINCTRL_H_
+
+#define TI_PIN_DEFAULT  0x0
+#define TI_PIN_AF1      0x1
+#define TI_PIN_AF2      0x2
+#define TI_PIN_AF3      0x3
+#define TI_PIN_AF4      0x4
+#define TI_PIN_AF5      0x5
+
+#endif /*ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_TI_HERCULES_PINCTRL_H_*/
