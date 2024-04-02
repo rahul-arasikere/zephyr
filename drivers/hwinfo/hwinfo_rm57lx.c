15a7d3794b3d92060bf3fd3c547b7d2e32ec579b
Rahul Arasikere <arasikere.rahul@gmail.com>
2024-04-01 21:41:40 -0400

    finished hwinfo driver rm57lx

--- drivers/hwinfo/hwinfo_rm57lx.c
+++ drivers/hwinfo/hwinfo_rm57lx.c
@@ -5,23 +5,73 @@
 
 #include <zephyr/drivers/hwinfo.h>
 
+#define SYS_EXCEPTION (*(volatile uint32_t *)0xffffffe4U)
+#define DEVICE_ID_REV       (*(volatile uint32_t *)0xfffffff0U)
+
+enum rm57lx_reset_bits {
+	POWERON_RESET = 0x8000U,
+	OSC_FAILURE_RESET = 0x4000U,
+	WATCHDOG_RESET = 0x2000U,
+	WATCHDOG2_RESET = 0x1000U,
+	DEBUG_RESET = 0x0800U,
+	INTERCONNECT_RESET = 0x0080U,
+	CPU0_RESET = 0x0020U,
+	SW_RESET = 0x0010U,
+	EXT_RESET = 0x0008U,
+	NO_RESET = 0x0000U
+};
+
 ssize_t z_impl_hwinfo_get_device_id(uint8_t *buffer, size_t length)
 {
-	return -ENOSYS;
+    uint32_t id = DEVICE_ID_REV;
+    if(length > sizeof(id)) {
+        length = sizeof(id);
+    } 
+    memcpy(buffer, &id, length);
+	return length;
 }
 
 int z_impl_hwinfo_get_reset_cause(uint32_t *cause)
 {
-	return -ENOSYS;
+	uint32_t flags = 0;
+	if ((SYS_EXCEPTION & (uint32_t)POWERON_RESET) != 0U) {
+		flags = RESET_POR;
+		SYS_EXCEPTION |= 0x0000FFFFU;
+	} else if ((SYS_EXCEPTION & (uint32_t)EXT_RESET) != 0U) {
+		SYS_EXCEPTION = (uint32_t)EXT_RESET;
+		if ((SYS_EXCEPTION & (uint32_t)OSC_FAILURE_RESET) != 0U) {
+			flags = RESET_CLOCK;
+			SYS_EXCEPTION |= (uint32_t)OSC_FAILURE_RESET;
+		} else if ((SYS_EXCEPTION &
+			    ((uint32_t)WATCHDOG_RESET | (uint32_t)WATCHDOG2_RESET)) != 0U) {
+			flags = RESET_WATCHDOG;
+			SYS_EXCEPTION |= (uint32_t)(WATCHDOG_RESET | WATCHDOG2_RESET);
+		} else if ((SYS_EXCEPTION & (uint32_t)SW_RESET) != 0U) {
+			flags = RESET_SOFTWARE;
+			SYS_EXCEPTION |= (uint32_t)SW_RESET;
+		} else {
+			flags = RESET_HARDWARE;
+		}
+	} else if ((SYS_EXCEPTION & (uint32_t)DEBUG_RESET) != 0U) {
+		flags = RESET_DEBUG;
+		SYS_EXCEPTION |= (uint32_t)DEBUG_RESET;
+	} else if ((SYS_EXCEPTION & (uint32_t)CPU0_RESET) != 0U) {
+		flags = RESET_HARDWARE;
+		SYS_EXCEPTION = (uint32_t)CPU0_RESET;
+	}
+	*cause = flags;
+	return 0;
 }
 
 int z_impl_hwinfo_clear_reset_cause(void)
 {
-	return -ENOSYS;
+	SYS_EXCEPTION = (uint32_t)UINT32_MAX;
+	return 0;
 }
 
 int z_impl_hwinfo_get_supported_reset_cause(uint32_t *supported)
 {
-	return -ENOSYS;
+	*supported = (RESET_POR | RESET_CLOCK | RESET_WATCHDOG | RESET_DEBUG | RESET_HARDWARE |
+		      RESET_SOFTWARE);
+	return 0;
 }
-
