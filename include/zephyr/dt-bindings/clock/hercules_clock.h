6a1f6c605de0d6e49fbe5cd72f57196319d536d0
Rahul Arasikere <arasikere.rahul@gmail.com>
2024-04-17 21:03:19 -0400

    updated dts

---
+++ include/zephyr/dt-bindings/clock/hercules_clock.h
@@ -0,0 +1,13 @@
+/**
+ * Copyrights (c) 2024 Rahul Arasikere <arasikere.rahul@gmail.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_HERCULES_CLOCK_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_HERCULES_CLOCK_H_
+
+enum ti_hercules_clocks {
+    OSCIN = 0,
+};
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_HERCULES_CLOCK_H_ */
