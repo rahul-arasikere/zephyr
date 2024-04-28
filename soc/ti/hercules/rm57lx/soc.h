5ed6a969a2c007f92b428df38f3a9e7445814846
Rahul Arasikere <arasikere.rahul@gmail.com>
2024-04-28 18:56:58 -0400

    pcr registers

--- soc/ti/hercules/rm57lx/soc.h
+++ soc/ti/hercules/rm57lx/soc.h
@@ -6,6 +6,8 @@
 #ifndef TI_HERCULES_RM57LX_SOC_H_
 #define TI_HERCULES_RM57LX_SOC_H_
 
+#include <zephyr/types.h>
+
 volatile struct hercules_syscon_1_regs {
 	/* 0xFFFFFF00U */
 	uint32_t SYSPC1;      /* 0x0000 */
@@ -78,24 +80,80 @@ volatile struct hercules_syscon_2_regs {
 	uint32_t PLLCTL3;     /* 0x0000 */
 	uint32_t rsvd1;       /* 0x0004 */
 	uint32_t STCCLKDIV;   /* 0x0008 */
-	uint32_t rsvd2[6U];   /* 0x000C */
+	uint32_t rsvd2[6];    /* 0x000C */
 	uint32_t ECPCNTL;     /* 0x0024 */
 	uint32_t ECPCNTL1;    /* 0x0028 */
-	uint32_t rsvd3[4U];   /* 0x002C */
+	uint32_t rsvd3[4];    /* 0x002C */
 	uint32_t CLK2CNTRL;   /* 0x003C */
 	uint32_t VCLKACON1;   /* 0x0040 */
-	uint32_t rsvd4[4U];   /* 0x0044 */
+	uint32_t rsvd4[4];    /* 0x0044 */
 	uint32_t HCLKCNTL;    /* 0x0054 */
-	uint32_t rsvd5[6U];   /* 0x0058 */
+	uint32_t rsvd5[6];    /* 0x0058 */
 	uint32_t CLKSLIP;     /* 0x0070 */
 	uint32_t rsvd6;       /* 0x0074 */
 	uint32_t IP1ECCERREN; /* 0x0078 */
-	uint32_t rsvd7[28U];  /* 0x007C */
+	uint32_t rsvd7[28];   /* 0x007C */
 	uint32_t EFC_CTLEN;   /* 0x00EC */
 	uint32_t DIEIDL_REG0; /* 0x00F0 */
 	uint32_t DIEIDH_REG1; /* 0x00F4 */
 	uint32_t DIEIDL_REG2; /* 0x00F8 */
 	uint32_t DIEIDH_REG3; /* 0x00FC */
+};
+
+volatile struct hercules_pcr_regs {
+	uint32_t PMPROTSET0;    /* 0x0000 */
+	uint32_t PMPROTSET1;    /* 0x0004 */
+	uint32_t rsvd1[2];      /* 0x0008 */
+	uint32_t PMPROTCLR0;    /* 0x0010 */
+	uint32_t PMPROTCLR1;    /* 0x0014 */
+	uint32_t rsvd2[2];      /* 0x0018 */
+	uint32_t PPROTSET0;     /* 0x0020 */
+	uint32_t PPROTSET1;     /* 0x0024 */
+	uint32_t PPROTSET2;     /* 0x0028 */
+	uint32_t PPROTSET3;     /* 0x002C */
+	uint32_t rsvd3[4];      /* 0x0030 */
+	uint32_t PPROTCLR0;     /* 0x0040 */
+	uint32_t PPROTCLR1;     /* 0x0044 */
+	uint32_t PPROTCLR2;     /* 0x0048 */
+	uint32_t PPROTCLR3;     /* 0x004C */
+	uint32_t rsvd4[4];      /* 0x0050 */
+	uint32_t PCSPWRDWNSET0; /* 0x0060 */
+	uint32_t PCSPWRDWNSET1; /* 0x0064 */
+	uint32_t rsvd5[2];      /* 0x0068 */
+	uint32_t PCSPWRDWNCLR0; /* 0x0070 */
+	uint32_t PCSPWRDWNCLR1; /* 0x0074 */
+	uint32_t rsvd6[2];      /* 0x0078 */
+	uint32_t PSPWRDWNSET0;  /* 0x0080 */
+	uint32_t PSPWRDWNSET1;  /* 0x0084 */
+	uint32_t PSPWRDWNSET2;  /* 0x0088 */
+	uint32_t PSPWRDWNSET3;  /* 0x008C */
+	uint32_t rsvd7[4];      /* 0x0090 */
+	uint32_t PSPWRDWNCLR0;  /* 0x00A0 */
+	uint32_t PSPWRDWNCLR1;  /* 0x00A4 */
+	uint32_t PSPWRDWNCLR2;  /* 0x00A8 */
+	uint32_t PSPWRDWNCLR3;  /* 0x00AC */
+	uint32_t rsvd8[4];      /* 0x00B0 */
+	uint32_t PDPWRDWNSET;   /* 0x00C0 */
+	uint32_t PDPWRDWNCLR;   /* 0x00C4 */
+	uint32_t rsvd9[78];     /* 0x00C8 */
+	uint32_t MSTIDWRENA;    /* 0x0200 */
+	uint32_t MSTIDENA;      /* 0x0204 */
+	uint32_t MSTIDDIAGCTRL; /* 0x0208 */
+	uint32_t rsvd10[61];    /* 0x020C */
+	struct {
+		uint32_t PSxMSTID_L;
+		uint32_t PSxMSTID_H;
+	} PSxMSTID[32]; /* 0x0300 */
+	struct {
+		uint32_t PPSxMSTID_L;
+		uint32_t PPSxMSTID_H;
+	} PPSxMSTID[8]; /* 0x0400 */
+	struct {
+		uint32_t PPSExMSTID_L;
+		uint32_t PPSExMSTID_H;
+	} PPSExMSTID[32];       /* 0x0440 */
+	uint32_t PCSxMSTID[32]; /* 0x0540 */
+	uint32_t PPCSxMSTID[8]; /* 0x05C0 */
 };
 
 #endif /* TI_HERCULES_RM57LX_SOC_H_ */
