77bc9ed568ea6e1a6910107b5b23de01d755acbd
Rahul Arasikere <arasikere.rahul@gmail.com>
2024-04-29 21:28:23 -0400

    cleanup macros

--- drivers/clock_control/clock_control_ti_hercules.c
+++ drivers/clock_control/clock_control_ti_hercules.c
@@ -108,15 +108,12 @@ static int ti_hercules_gcm_clock_init(const struct device *dev)
 	/* Disable Bypass on Slip */
 	pll1_conf |= 0x2 << 29;
 #endif /* Bypass-On-PLL-Slip */
-	BUILD_ASSERT(z_plldiv(PLL1_NODE) >= 1 && z_plldiv(PLL1_NODE) <= 32,
-		     "R out of range! (1 -32)");
+	BUILD_ASSERT(IN_RANGE(z_plldiv(PLL1_NODE), 1, 32), "R out of range! (1 -32)");
 	pll1_conf |= (z_plldiv(PLL1_NODE) - 1) << 24;
 	pll1_conf |= DT_PROP_BY_IDX(PLL1_NODE, reset_on_oscillator_fail) << 23;
-	BUILD_ASSERT(z_refclkdiv(PLL1_NODE) >= 1 && z_refclkdiv(PLL1_NODE) <= 64,
-		     "NR out of range! (1 - 64)");
+	BUILD_ASSERT(IN_RANGE(z_refclkdiv(PLL1_NODE), 1, 64), "NR out of range! (1 - 64)");
 	pll1_conf |= (z_refclkdiv(PLL1_NODE) - 1) << 16;
-	BUILD_ASSERT(z_pllmul(PLL1_NODE) >= 1 && z_pllmul(PLL1_NODE) <= 256,
-		     "NF out of range! (1 - 256)");
+	BUILD_ASSERT(IN_RANGE(z_pllmul(PLL1_NODE), 1, 256), "NF out of range! (1 - 256)");
 	pll1_conf |= (z_pllmul(PLL1_NODE) - 1) << 8;
 
 	sys_regs_1->PLLCTL1 = pll1_conf;
@@ -124,35 +121,27 @@ static int ti_hercules_gcm_clock_init(const struct device *dev)
 	uint32_t pll2_conf = 0;
 #if FMENA
 	pll2_conf |= BIT(31);
-	BUILD_ASSERT(z_pll1_spreadingrate() >= 1 && z_pll1_spreadingrate() <= 512,
-		     "NS out of range! (1-512)");
+	BUILD_ASSERT(IN_RANGE(z_pll1_spreadingrate(), 1, 512), "NS out of range! (1-512)");
 	pll2_conf |= (z_pll1_spreadingrate() - 1) << 22;
-	BUILD_ASSERT(z_pll1_mulmod() >= 8 && z_pll1_mulmod() <= 511,
-		     "MULMOD out of range! (8 - 511)");
+	BUILD_ASSERT(IN_RANGE(z_pll1_mulmod(), 8, 511), "MULMOD out of range! (8 - 511)");
 	pll2_conf |= z_pll1_mulmod() << 12;
-	BUILD_ASSERT(z_pll1_spreadingamount() >= 1 && z_pll1_spreadingamount() <= 512,
-		     "NV out of range! ( 1 - 512)");
+	BUILD_ASSERT(IN_RANGE(z_pll1_spreadingamount(), 1, 512), "NV out of range! (1 - 512)");
 	pll2_conf |= (z_pll1_spreadingamount() - 1);
 #endif /* FMENA */
-	BUILD_ASSERT(z_odpll(PLL1_NODE) >= 1 && z_odpll(PLL1_NODE) <= 8,
-		     "OD out of range! (1 - 8)");
+	BUILD_ASSERT(IN_RANGE(z_odpll(PLL1_NODE), 1, 8), "OD out of range! (1 - 8)");
 	pll2_conf |= (z_odpll(PLL1_NODE) - 1) << 9;
 	sys_regs_1->PLLCTL2 = pll2_conf;
 #endif /* PLL1_NODE */
 
 #if IS_ENABLED(PLL2_NODE)
 	uint32_t pll3_conf = 0;
-	BUILD_ASSERT(z_odpll(PLL2_NODE) >= 1 && z_odpll(PLL2_NODE) <= 8,
-		     "OD out of range! (1 - 8)");
+	BUILD_ASSERT(IN_RANGE(z_odpll(PLL2_NODE), 1, 8), "OD out of range! (1 - 8)");
 	pll3_conf |= (z_odpll(PLL2_NODE) - 1) << 29;
-	BUILD_ASSERT(z_plldiv(PLL2_NODE) >= 1 && z_plldiv(PLL2_NODE) <= 32,
-		     "R out of range! (1 -32)");
+	BUILD_ASSERT(IN_RANGE(z_plldiv(PLL2_NODE), 1, 32), "R out of range! (1 -32)");
 	pll3_conf |= (z_plldiv(PLL2_NODE) - 1) << 24;
-	BUILD_ASSERT(z_refclkdiv(PLL2_NODE) >= 1 && z_refclkdiv(PLL2_NODE) <= 64,
-		     "NR out of range! (1 - 64)");
+	BUILD_ASSERT(IN_RANGE(z_refclkdiv(PLL2_NODE), 1, 64), "NR out of range! (1 - 64)");
 	pll3_conf |= (z_refclkdiv(PLL2_NODE) - 1) << 16;
-	BUILD_ASSERT(z_pllmul(PLL2_NODE) >= 1 && z_pllmul(PLL2_NODE) <= 256,
-		     "NF out of range! (1 - 256)");
+	BUILD_ASSERT(IN_RANGE(z_pllmul(PLL2_NODE), 1, 256), "NF out of range! (1 - 256)");
 	pll3_conf |= (z_pllmul(PLL2_NODE) - 1) << 8;
 	sys_regs_2->PLLCTL3 = pll3_conf;
 #endif /* PLL2_NODE */
